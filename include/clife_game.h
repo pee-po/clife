7a497d7b1f31f3774a8d1a51312c78530b296493
ppo <piotr.pochec@interia.pl>
2023-10-16 00:12:42 +0200

    Game zero (#1)
    Add zero-version
    
    Some of the features:
    * Add game's interface: memory management, rule setting, getters, setters, serialisation and step calculations.
    * Make the life object opaque
    * Add cmake/ctest framework
    * Add tests: serialisation, deserialisation, smoke

---
+++ include/clife_game.h
@@ -0,0 +1,178 @@
+#ifndef _clife_game_h_include_
+#define _clife_game_h_include_
+
+#include <stdint.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+/*
+ * Type definitions
+ */
+typedef struct clife_s clife_t;
+
+struct clife_point_state{
+    bool state;
+    uint32_t x;
+    uint32_t y;
+};
+
+typedef enum {OK, BUFF_SHORT, ERR} update_status;
+
+/*
+ * Macros
+ */
+#define clife_set_def_rule(x) clife_set_rule(x, 8, 12)
+
+/*
+ * Function definitions
+ */
+
+/* Memory */
+/**
+ * Construct game of life's object with reasonable defaults.
+ *
+ * The object is allocated on the heap - the ownership is paseed to the
+ * caller, it should be freed with delete_clife.
+ *
+ * @param[in] width grid's width given in number of cells
+ * @param[in] width grid's hight given in number of cells
+ * @param[out] life pointer to object;
+ */
+clife_t *new_clife(uint32_t width, uint32_t height);
+
+/**
+ * Destructs game of life's object and frees the memory.
+ *
+ * @param[in] life pointer to the object to be deallocated
+ */
+void delete_clife(clife_t *life);
+
+
+/* General */
+/**
+ * Perform step calculations
+ *
+ * @param[in] life pointer to the life object
+ */
+void clife_step(clife_t *life);
+
+/**
+ * Perform step calculations and fill a buffer with cell status updates.
+ *
+ * As opposed to clife_step - it doesn't use the secondary board
+ * (internal) representation. The cells that need to be updated are filled into
+ * state_buff (which has to be pre-allocated). If the state_buff is filled
+ * correctly, then the updates are applied to the internal board
+ * representation. If the buffer is NULL or is shorter than the update
+ * then BUFF_SHORT is returned and the updates are NOT applied to the board.
+ * *update_len starts at 0. The state updates are placed into the buffer
+ * starting at index = 0. Each time this is done, *update_len is incremented.
+ * At the end *update_len is the number of cell state
+ * updates reliably recorded into the buffer. The actual number of elements in
+ * buffer that were modified may differ - caller should rely only on elements
+ * of buffer indexed by a number smaller than *update_len.
+ *
+ * @param[in] life pointer to the life object
+ * @param[in] state_buff status update buffer
+ * (the ownership remains with the caller)
+ * @param[in] buff_len buffer size (number of records)
+ * @param[in] update_len pointer to a variable that will be set
+ * to the number of records in buffer that have been modified.
+ * (the ownership remains with the caller)
+ * @param[out] update_status 
+ */
+update_status clife_step_get_updates(
+    clife_t *life,
+    struct clife_point_state *state_buff,
+    uint64_t buff_len,
+    uint64_t *update_len
+);
+
+
+/* Getters and setters */
+/**
+ * Get life object's width.
+ *
+ * @param[in] life pointer to the life object
+ */
+uint32_t clife_get_width(clife_t *life);
+
+/**
+ * Get life object's height.
+ *
+ * @param[in] life pointer to the life object
+ */
+uint32_t clife_get_height(clife_t *life);
+
+/**
+ * Get cell's state.
+ *
+ * @param[in] x cell's column
+ * @param[in] y cell's row
+ * @param[out] state cell's state
+ */
+bool clife_get_cell(clife_t *life, uint32_t x, uint32_t y);
+
+/**
+ * Set cell's state.
+ *
+ * @param[in] x cell's column
+ * @param[in] y cell's row
+ * @param[in] state cell's new state
+ */
+void clife_set_cell(clife_t *life, uint32_t x, uint32_t y, bool state);
+
+/**
+ * Set a basic rule for game of life.
+ *
+ * Set's a rule in form of BX/SY where X and Y are encoded to rule_b and rule_s
+ * in following way: if rule R includes positive integer Z then Z-th bit of R
+ * is set. E.g. the cannonical rule of B3/S23 is encoded as:
+ * rule_b = 2^3 = 8; rule_s = 2^2 + 2^3 = 12.
+ *
+ * @param[in] life pointer to the life object
+ * @param[in] rule_b unsigned integer representing "born" rule in such a way
+ * that if n-th bit is set then n alive neighbours set the cell alive - if 
+ * dead.
+ * @param[in] rule_s unsigned integer representing "survive" rule in such a way
+ * that if n-th bit is set then n alive neighbours keep the cell alive - if 
+ * already alive.
+ */
+bool clife_set_rule(clife_t *life, uint16_t rule_b, uint16_t rule_s);
+
+
+/* Serialisation */
+/**
+ * Serilise life object to a bytestream.
+ *
+ * Transcribes only cell states in a reversible way, that is - given other
+ * members of life object are known, the resulting bytestream identifies
+ * the board state uniquely. A reverse operation is clife_deserialise.
+ *
+ * @param[in] life pointer to the life object
+ * @param[in] buffer an allocated char buffer. Ownership of the buffer remains
+ * with the caller
+ * @param[in] buff_len length of buffer
+ * @param[out] size number of bytes written to the buffer. A value of 0
+ * indicates that the size of buffer was insufficient.
+ */
+size_t clife_serialise(clife_t *life, uint8_t *buffer, size_t buff_len);
+
+/**
+ * Deserialise bytestream into life object.
+ *
+ * Sets cell states according to the information in bytestream.
+ * A reverse operation is clife_serialise.
+ *
+ * @param[in] life pointer to the life object
+ * @param[in] buffer an char buffer (read only)
+ * @param[in] buff_len length of buffer
+ * @param[out] bytes_processed number of bytes processed. Generally a
+ * successful operation will have bytes_processed == buff_len. But this doesn't
+ * safeguard against a situation where the buffer is to small compared to the
+ * board and therfore not all cells are set.
+ */
+size_t clife_deserialise(clife_t *life, uint8_t *buffer, size_t buff_len);
+
+#endif /* _clife_game_h_include_ */
+
---
+++ src/clife_game.c
@@ -0,0 +1,333 @@
+#include "clife_game.h"
+
+struct clife_s {
+    bool *board;
+    bool *board_next;
+    uint32_t width;
+    uint32_t height;
+    uint16_t rule_b;
+    uint16_t rule_s;
+    bool *lookup_table;
+};
+
+/*
+ * Declaration of internal functions
+ */
+
+/**
+ * Generate a lookup table for determinig cell's next state
+ *
+ * The ownership is passed to the caller, needs to freed.
+ * The table has following form: if the neighbourhood has state n then
+ * n-th entry gives the midlle cells next state. State n is an unsigned integer
+ * where LSB is the state of top-left neighbour of middle cell (alive == true);
+ * next bit is the state of top neighbour, and so on (row by row).
+ *
+ * @param[in] life pointer to the life object.
+ */
+bool *generate_lookup_table(clife_t *life);
+
+/**
+ * Get neighbours of a given cell
+ *
+ * Returns neighbourhood state, where LSB is the state is top-left neighbour
+ * of cell given by (col, row) (alize == true); next bit is the state of
+ * top nwighbour, and so on (row by row).
+ *
+ * @param[in] life pointer to the life object.
+ * @param[in] col horizontal position of the cell central to the
+ * neighbourhood to be calculated.
+ * @param[in] row vertical position of the cell central to the
+ * neighbourhood to be calculated.
+ */
+uint16_t get_neighbours(clife_t *life, uint32_t col, uint32_t row);
+
+
+/*
+ * Exported functions
+ */
+
+/* Memory */
+clife_t *new_clife(uint32_t width, uint32_t height) {
+    clife_t *life;
+    bool *board, *board_sec;
+
+    size_t cell_cnt = (size_t) width * (size_t) height;
+    size_t board_size = sizeof *board * cell_cnt;
+
+    /* Allocations */
+    life = malloc(sizeof *life);
+    board = malloc(board_size);
+    board_sec = malloc(board_size);
+
+    /* Check allocations */
+    if (life == NULL || board == NULL || board_sec == NULL) {
+        delete_clife(life);
+        return NULL;
+    }
+
+    for (size_t i = 0; i < cell_cnt; i++) {
+        board[i] = false;
+        board_sec[i] = false;
+    }
+
+    /* Construct struct */
+    life->board = board;
+    life->board_next = board_sec;
+    life->width = width;
+    life->height = height;
+    life->rule_b = 0;
+    life->rule_s = 0;
+    life->lookup_table = NULL;
+    
+    return life;
+} /* End new_clife */
+
+void delete_clife(clife_t *life) {
+    if (life != NULL) {
+        free(life->board);
+        free(life->board_next);
+        free(life->lookup_table);
+    }
+    free(life);
+} /* End delete_clife */
+
+void clife_step(clife_t *life) {
+    uint16_t neighbourhood_state;
+    size_t cell_offset;
+    bool state;
+    for (uint32_t row = 0; row < life->height; row++) {
+        for (uint32_t col = 0; col < life->width; col++) {
+            neighbourhood_state = get_neighbours(life, col, row);
+            cell_offset = col + row*life->width;
+            state = *(life->lookup_table + neighbourhood_state);
+            *(life->board_next + cell_offset) = state;
+        }
+    }
+    bool *temp_board;
+    temp_board = life->board;
+    life->board = life->board_next;
+    life->board_next = temp_board;
+} /* End clife_step */
+
+update_status clife_step_get_updates(
+    clife_t *life,
+    struct clife_point_state *state_buff,
+    uint64_t buff_len,
+    uint64_t *update_len
+) {
+    if (state_buff == NULL || !buff_len) {
+        return BUFF_SHORT;
+    }
+    /* Find cells that change state */
+    *update_len = 0;
+    uint16_t neighbourhood_state;
+    size_t cell_offset;
+    bool new_state, old_state;
+    struct clife_point_state *state_buff_runner = state_buff;
+    for (uint32_t row = 0; row < life->height; row++) {
+        for (uint32_t col = 0; col < life->width; col++) {
+            neighbourhood_state = get_neighbours(life, col, row);
+            cell_offset = col + row*life->width;
+            old_state = *(life->board + cell_offset);
+            new_state = *(life->lookup_table + neighbourhood_state);
+            if (!old_state != !new_state) {
+                if (*update_len > (buff_len - 1)) {
+                    return BUFF_SHORT;
+                }
+                state_buff_runner->state = new_state;
+                state_buff_runner->x=col;
+                state_buff_runner->y=row;
+                state_buff_runner++;
+                (*update_len)++;
+            }
+        }
+    }
+    /* Introduce changes to the board */
+    if (*update_len) {
+        state_buff_runner = state_buff;
+        struct clife_point_state *state_buff_end = state_buff + *update_len;
+        while (state_buff_runner < state_buff_end) {
+            clife_set_cell(
+                life,
+                state_buff_runner->x,
+                state_buff_runner->y,
+                state_buff_runner->state
+            );
+            state_buff_runner++;
+        }
+    }
+    return OK;
+}
+
+
+/* Getters and setters */
+uint32_t clife_get_width(clife_t *life) {
+    return life->width;
+} /* End clife_get_width */
+
+uint32_t clife_get_height(clife_t *life) {
+    return life->height;
+} /* End clife_get_height */
+
+bool clife_get_cell(clife_t *life, uint32_t x, uint32_t y) {
+    size_t offset = x + life->width * y;
+    return *(life->board + offset);
+} /* End clife_get_cell */
+
+void clife_set_cell(clife_t *life, uint32_t x, uint32_t y, bool state) {
+    size_t offset = x + life->width * y;
+    *(life->board + offset) = state;
+} /* End clife_set_cell */
+
+bool clife_set_rule(clife_t *life, uint16_t rule_b, uint16_t rule_s) {
+    life->rule_b = rule_b;
+    life->rule_s = rule_s;
+    bool *lookup_table = generate_lookup_table(life);
+    if (lookup_table == NULL) {
+        return false;
+    } else {
+        free(life->lookup_table);
+        life->lookup_table = lookup_table;
+        return true;
+    }
+} /* End clife_set_rule */
+
+
+/* Serialisation */
+size_t clife_serialise(clife_t *life, uint8_t *buffer, size_t buff_len) {
+    uint32_t max_col = life->width - 1;
+    size_t copied_cnt = 0;
+    bool state;
+
+    uint8_t cycler = 7;
+    uint8_t acc = 0;
+    for (uint32_t row = 0; row < life->height; row++) {
+        for (uint32_t col = 0; col <= max_col; col++) {
+            if (copied_cnt >= buff_len) return 0;
+            /*
+             * Uses only an exported function to access cell state.
+             * This is slow but decouples this function from board
+             * implementation.
+             */
+            state = clife_get_cell(life, col, row);
+            if (state) acc = acc | (1<<cycler);
+            if (cycler == 0 || col == max_col) {
+                *buffer = acc;
+                buffer++;
+                acc = 0;
+                cycler = 7;
+                copied_cnt++;
+            } else {
+                cycler--;
+            }
+        }
+    }
+    return copied_cnt;
+} /* End clife_serialise */
+
+size_t clife_deserialise(clife_t *life, uint8_t *buffer, size_t buff_len) {
+    size_t bytes_processed = 0;
+    uint32_t col = 0;
+    uint32_t row = 0;
+    bool state;
+
+    while (buff_len) {
+        for (int8_t i = 7; i >= 0; i--) {
+            /* ternary expresion is to keep bools clean */
+            state = *buffer & (1<<i) ? true : false;
+            /*
+             * Uses only an exported function to access cell state.
+             * This is slow but decouples this function from board
+             * implementation.
+             */
+            clife_set_cell(life, col, row, state);
+            col++;
+            if (col >= life->width) {
+                col = 0;
+                row++;
+                if (row > life->height) {
+                    return bytes_processed;
+                }
+                break;
+            }
+        }
+        buffer++;
+        bytes_processed++;
+        buff_len--;
+    }
+    return bytes_processed;
+} /* End clife_deserialise */
+
+
+/*
+ * Internal functions
+ */
+
+bool *generate_lookup_table(clife_t *life) {
+    uint16_t rule_b = life->rule_b;
+    uint16_t rule_s = life->rule_s;
+    uint16_t neighbourhood_size = 9; // 3x3;
+    uint16_t combination_cnt = 512; // 2^neighbourhood_size;
+
+    bool *table, *record;
+    table = malloc(sizeof *table * combination_cnt);
+    if (table == NULL) {
+        return NULL;
+    }
+
+    uint16_t alive_cnt;
+    for (uint16_t nstate = 0; nstate < combination_cnt; nstate ++) {
+        bool mid_cell_alive = nstate & (1<<4);
+        alive_cnt = 0;
+        for (uint16_t cell = 0; cell < neighbourhood_size; cell++) {
+            if (cell == 4) continue;
+            if (nstate & (1<<cell)) alive_cnt++;
+        }
+
+        record = table + nstate;
+        if (mid_cell_alive) {
+            if (rule_s & (1<<alive_cnt)) {
+                *record = true;
+            } else {
+                *record = false;
+            }
+        } else {
+            if (rule_b & (1<<alive_cnt)) {
+                *record = true;
+            } else {
+                *record = false;
+            }
+        }
+    }
+    return table;
+} /* End generate_lookup_table */
+
+uint16_t get_neighbours(clife_t *life, uint32_t col, uint32_t row) {
+    int16_t col_offset, row_offset;
+    const uint32_t min_col = 0;
+    const uint32_t max_col = life->width - 1;
+    const uint32_t min_row = 0;
+    const uint32_t max_row = life->height - 1;
+    uint16_t neighbourhood = 0;
+    size_t offset;
+
+    for (uint16_t i = 0; i < 9; i++) {
+        col_offset = i % 3 - 1;
+        row_offset = i / 3 - 1;
+
+        if (
+            (col > min_col || col_offset >= 0)
+            && (col < max_col || col_offset <= 0)
+            && (row > min_row || row_offset >= 0)
+            && (row < max_row || row_offset <= 0)
+        ) {
+            offset = col + col_offset + (row + row_offset)*life->width;
+            if (*(life->board + offset)) {
+                neighbourhood = neighbourhood | 1<<i;
+            }
+        }
+    }
+    return neighbourhood;
+} /* End get_neighbours */
+
