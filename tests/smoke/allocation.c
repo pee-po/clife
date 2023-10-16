449bb5673ef38db6dd04649d592afb45e9d83ba9
Piotr Pocheć <piotr.pochec@interia.pl>
2023-10-16 13:31:12 +0200

    Parametrize allocation tests

--- tests/smoke/allocation.c
+++ tests/smoke/allocation.c
@@ -1,11 +1,22 @@
 #include "clife_game.h"
+#include <errno.h>
+
+void parse_args(int argc, char **argv, uint32_t *width, uint32_t *height);
 
 int main(int argc, char **argv) {
-    (void) argc;
-    (void) argv;
+    uint32_t width, height;
+    parse_args(argc, argv, &width, &height);
 
-    clife_t *life = new_clife(3, 3);
+    clife_t *life = new_clife(width, height);
     delete_clife(life);
 
     return 0;
+}
+
+void parse_args(int argc, char **argv, uint32_t *width, uint32_t *height) {
+    if (argc != 3) exit(1);
+    *width = strtoul(argv[1], NULL, 10);
+    *height = strtoul(argv[2], NULL, 10);
+
+    if (errno) exit(2);
 }
