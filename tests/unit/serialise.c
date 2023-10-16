dee8ee60fee3124c0a87eae0fc95ac8357428365
Piotr Pocheć <piotr.pochec@interia.pl>
2023-10-16 13:48:15 +0200

    Move arg parsing to test utils
    Argument parsing is similar in different tests.
    Renamed to state_args and moved to utils.

--- tests/unit/serialise.c
+++ tests/unit/serialise.c
@@ -1,30 +1,17 @@
 #include "clife_game.c"
 #include "test_utils.c"
-#include <errno.h>
 #include <stdio.h>
 #include <string.h>
 
 #define BUFF_LEN 1024
 
-struct parsed_args {
-    char *expected_hexstr;
-    uint32_t width;
-    uint32_t height;
-    uint32_t *x_arr;
-    uint32_t *y_arr;
-    size_t num_points;
-};
-
-/* CLI parsing and cleanup */
-struct parsed_args *parse_args(int argc, char **argv);
-void free_args(struct parsed_args *args);
 /* Compare a buffer as produced by serialisation to a hexstring */
 int compare_byte_hex(uint8_t *byte_buff, char *hex_buff, size_t buff_len);
 /* helper for above */
 uint8_t hex2byte(char *start);
 
 int main(int argc, char **argv) {
-    struct parsed_args *args = parse_args(argc, argv);
+    struct parsed_state_args *args = parse_state_args(argc, argv);
 
     clife_t *life = new_clife(args->width, args->height);
     size_t offset;
@@ -45,49 +32,6 @@ int main(int argc, char **argv) {
     if (num_bytes*2 != strlen(args->expected_hexstr)) return 1;
 
     return compare_byte_hex(buffer, args->expected_hexstr, num_bytes);
-}
-
-
-struct parsed_args *parse_args(int argc, char **argv) {
-    if (argc < 6 || argc % 2) exit(1);
-    struct parsed_args *args;
-    args = malloc(sizeof *args);
-    if (args == NULL) exit(2);
-
-    args->expected_hexstr = argv[1];
-    args->num_points = (argc - 4) / 2;
-    args->x_arr = malloc(sizeof args->x_arr * args->num_points);
-    args->y_arr = malloc(sizeof args->y_arr * args->num_points);
-    if (args->x_arr == NULL || args->y_arr == NULL) {
-        free_args(args);
-        exit(2);
-    }
-
-    int argci = 4;
-    int ind = 0;
-    while (argci < argc) {
-        args->x_arr[ind] = strtoul(argv[argci], NULL, 10);
-        args->y_arr[ind] = strtoul(argv[argci + 1], NULL, 10);
-        argci += 2;
-        ind++;
-    }
-    args->width = strtoul(argv[2], NULL, 10);
-    args->height = strtoul(argv[3], NULL, 10);
-
-    if (errno) {
-        free_args(args);
-        exit(2);
-    }
-    return args;
-}
-
-void free_args(struct parsed_args *args) {
-    if (args != NULL) {
-        free(args->expected_hexstr);
-        free(args->x_arr);
-        free(args->y_arr);
-        free(args);
-    }
 }
 
 int compare_byte_hex(uint8_t *byte_buff, char *hex_buff, size_t buff_len) {
--- tests/utils/test_utils.c
+++ tests/utils/test_utils.c
@@ -1,3 +1,18 @@
+#include <errno.h>
+
+struct parsed_state_args {
+    char *expected_hexstr;
+    uint32_t width;
+    uint32_t height;
+    uint32_t *x_arr;
+    uint32_t *y_arr;
+    size_t num_points;
+};
+
+/* CLI parsing and cleanup */
+struct parsed_state_args *parse_state_args(int argc, char **argv);
+void free_state_args(struct parsed_state_args *args);
+
 uint8_t hex2byte(char *start) {
     uint8_t byte = 0;
     for (int i = 0; i < 2; i++) {
@@ -13,4 +28,46 @@ uint8_t hex2byte(char *start) {
         start++;
     }
     return byte;
+}
+
+struct parsed_state_args *parse_state_args(int argc, char **argv) {
+    if (argc < 6 || argc % 2) exit(1);
+    struct parsed_state_args *args;
+    args = malloc(sizeof *args);
+    if (args == NULL) exit(2);
+
+    args->expected_hexstr = argv[1];
+    args->num_points = (argc - 4) / 2;
+    args->x_arr = malloc(sizeof args->x_arr * args->num_points);
+    args->y_arr = malloc(sizeof args->y_arr * args->num_points);
+    if (args->x_arr == NULL || args->y_arr == NULL) {
+        free_state_args(args);
+        exit(2);
+    }
+
+    int argci = 4;
+    int ind = 0;
+    while (argci < argc) {
+        args->x_arr[ind] = strtoul(argv[argci], NULL, 10);
+        args->y_arr[ind] = strtoul(argv[argci + 1], NULL, 10);
+        argci += 2;
+        ind++;
+    }
+    args->width = strtoul(argv[2], NULL, 10);
+    args->height = strtoul(argv[3], NULL, 10);
+
+    if (errno) {
+        free_state_args(args);
+        exit(2);
+    }
+    return args;
+}
+
+void free_state_args(struct parsed_state_args *args) {
+    if (args != NULL) {
+        free(args->expected_hexstr);
+        free(args->x_arr);
+        free(args->y_arr);
+        free(args);
+    }
 }
